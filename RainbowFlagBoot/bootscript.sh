bb2b967ca776a5102754c1f9961083cd555bc58d
Abhinav a.k.a Demkeys <abhinavabhinavgupta91@gmail.com>
2019-01-05 05:33:34 +0300

    Create bootscript.sh

---
+++ RainbowFlagBoot/bootscript.sh
@@ -0,0 +1,7 @@
+current_name="rainbow_flag"
+assemblyfile_name="$current_name.s"
+objectfile_name="$current_name.o"
+binaryfile_name="$current_name.bin"
+imagefile_name="$current_name.img"
+
+as $assemblyfile_name -o $objectfile_name && objcopy -O binary $objectfile_name $binaryfile_name && dd if=$binaryfile_name of=$imagefile_name && qemu-system-x86_64 -net none -drive format=raw,file=$imagefile_name
